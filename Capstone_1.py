416591e476c0b41319e1d137ec6576e0329ebd6c
BryanEHaley <48870585+BryanEHaley@users.noreply.github.com>
2019-11-08 04:00:17 -0800

    Add files via upload

--- Capstone_1.py
+++ Capstone_1.py
@@ -8,6 +8,11 @@ Created on Tue Oct 22 03:49:20 2019
 # This is the first part of my exploratory analysis and capstone for Springboard 
 import pandas as pd
 
+import numpy as np
+import seaborn as sns #visualisation
+import matplotlib.pyplot as plt #visualisation 
+sns.set(color_codes=True)
+
 #due to memory size, I loaded these individually and then commented out afterwords. 
 #jobs_11 = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\Jobs\Jobs_2011.xlsx')
 #jobs_12 = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\Jobs\Jobs_2012.xlsx')
@@ -17,6 +22,9 @@ import pandas as pd
 #jobs_16 = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\Jobs\Jobs_2016.xlsx')
 #jobs_17 = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\Jobs\Jobs_2017.xlsx')
 #jobs_18 = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\Jobs\Jobs_2018.xlsx')
+
+# uploading popualtion data from Us census 
+pop = pd.read_excel(r'C:\Users\Bryan\Documents\Capstone Job Data\pop-est2018-alldata.xlsx')
 
 # for 2011
 filtered_cities_11 = ['Boston-Cambridge-Quincy, MA-NH','New York-Northern New Jersey-Long Island, NY-NJ-PA','Chicago-Joliet-Naperville, IL Metropolitan Division','Detroit-Warren-Livonia, MI','San Francisco-San Mateo-Redwood City, CA Metropolitan Division','Seattle-Tacoma-Bellevue, WA','Austin-Round Rock-San Marcos, TX','Nashville-Davidson--Murfreesboro--Franklin, TN','Dallas-Fort Worth-Arlington, TX','Washington-Arlington-Alexandria, DC-VA-MD-WV','Denver-Aurora-Broomfield, CO','Baltimore-Towson, MD','Charlotte-Gastonia-Rock Hill, NC-SC','Philadelphia-Camden-Wilmington, PA-NJ-DE-MD','Los Angeles-Long Beach-Santa Ana, CA','Phoenix-Mesa-Glendale, AZ','Portland-Vancouver-Hillsboro, OR-WA','Atlanta-Sandy Springs-Marietta, GA','Indianapolis-Carmel, IN','Houston-Sugar Land-Baytown, TX']
@@ -103,11 +111,48 @@ types_17 = city_jobs_17[city_jobs_17['occ_code'].isin(job_codes)]
 types_18 = city_jobs_18[city_jobs_18['occ_code'].isin(job_codes)]
 
 # for all years 2011 - 2018
-jobs_11_18 = pd.concat([types_11,types_12,types_13,types_14,types_15,types_16,types_17,types_18])
+jobs_all_years = pd.concat([types_11,types_12,types_13,types_14,types_15,types_16,types_17,types_18],axis=0)
 
-jobs_all_years = types_11.append([types_12,types_13,types_14,types_15,types_16,types_17,types_18],ignore_index=True, sort=False)
+# for visuals and exploratory analysis 
+import seaborn as sns
+# convertig string to numeric
+types_11['A_MEAN'] = types_11['A_MEAN'].apply(pd.to_numeric,errors='coerce')
+types_12['a_mean'] = types_12['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_13['a_mean'] = types_13['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_14['a_mean'] = types_14['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_15['a_mean'] = types_15['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_16['a_mean'] = types_16['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_17['a_mean'] = types_17['a_mean'].apply(pd.to_numeric,errors='coerce')
+types_18['a_mean'] = types_18['a_mean'].apply(pd.to_numeric,errors='coerce')
 
+#pd.concat([types_11,types_12],ignore_Index ="True")
 
+#types_11.merge(types_12,how="left")
 
+#boxplot for mean salaries in 2011
+sns.boxplot(types_11['A_MEAN'])
 
+#sns.boxplot(types_18['a_mean'])
 
+#sns.boxplot(types_12['a_mean'])
+
+# a quick corrplot test for types_11
+types_11.corr(method='pearson')
+
+#convering to excel files to combine and use in Tableau, share via excel file
+#types_11.to_excel("job_types_11.xlsx")
+#types_12.to_excel("job_types_12.xlsx")
+#types_13.to_excel("job_types_13.xlsx")
+#types_14.to_excel("job_types_14.xlsx")
+#types_15.to_excel("job_types_15.xlsx")
+#types_16.to_excel("job_types_16.xlsx")
+#types_17.to_excel("job_types_17.xlsx")
+#types_18.to_excel("job_types_18.xlsx")
+
+#importing combined files (2011-2018)
+jobs_total = pd.read_excel(r'C:\Users\Bryan\.spyder-py3\job_types_11.18.xlsx')
+jobs_total['A_MEAN'] = jobs_total['A_MEAN'].apply(pd.to_numeric,errors='coerce')
+
+sns.boxplot(jobs_total['A_MEAN'])
+
+#ax = jobs_total.plot.bar(x = 'A_MEAN',rot=0)
